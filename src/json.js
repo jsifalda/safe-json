ee7453aed134c0edbe68df9a42137e6d320166e4
jsifalda <sifalda.jiri@gmail.com>
2015-06-08 13:11:48 +0200

    Ability to setup JSON wrapper and debug function

--- src/json.js
+++ src/json.js
@@ -1,22 +1,61 @@
-export default {
+let factory = (_wrapper = null, _debug = null) => {
 
-  stringify (value) {
+  let debug = (...args) => {
 
-    return JSON.stringify(value)
-
-  },
-
-  parse (value, defaults = null) {
-
-    try {
-
-      return JSON.parse(value)
-
-    } catch (error) {
-
-      console.warn('JSON.parse error', error)
-      return defaults
-
+    if (_debug) {
+      _debug(...args)
+    } else if (console && console.warn) {
+      console.warn(...args)
+    } else {
+      throw new Error('Please provide "debug" function. Console.log is not available in global scope')
     }
   }
+
+  let getWrapper = () => {
+
+    if (_wrapper) {
+      return _wrapper
+    } else if (JSON) {
+      return JSON
+    } else {
+      throw new Error('Please provide JSON wrapper. JSON is not available in global scope.')
+    }
+  }
+
+  const json = {
+
+    stringify (value, defaults = null) {
+
+      try {
+
+        return getWrapper().stringify(value)
+
+      } catch (error) {
+
+        debug('JSON.stringify error', error)
+        return defaults
+
+      }
+
+    },
+
+    parse (value, defaults = null) {
+
+      try {
+
+        return getWrapper().parse(value)
+
+      } catch (error) {
+
+        debug('JSON.parse error', error)
+        return defaults
+
+      }
+    }
+  }
+
+  return json
+
 }
+
+export default factory
